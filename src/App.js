1f8464ff8b078e13632c029739479e8282340034
Benatovich <sambenatovich@gmail.com>
2022-02-09 15:54:31 -0500

    App.js, fetch-utils, and PokePage should be set up properly. moving on to pokelist next

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import PokePage from './PokePage';
+import PokeDetail from './PokeDetail';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link className='home-button' to="/">Home</Link>
+        <Switch>
+          <Route exact path="/">
+            <PokePage />
+          </Route>
+          <Route exact path="/pokemon/:id">
+            <PokeDetail />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
