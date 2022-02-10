d7b8a44d7f9fa51d9c5787916a3b9f13e410bb09
Benatovich <sambenatovich@gmail.com>
2022-02-09 19:01:29 -0500

    removed a console.log

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export default function Pokemon({ pokemon }) {
-  console.log(pokemon, 'pokemon');
+//   console.log(pokemon, 'pokemon');
   return (
     <Link className='pokemon-link' key={pokemon.id} to={`/pokemon/${pokemon.id}`}>
       <div className='pokemon'>
