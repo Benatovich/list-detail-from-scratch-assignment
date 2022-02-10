6f0e9f94f22f6f7a6ebe75b140d2cabba1d81f5e
Benatovich <sambenatovich@gmail.com>
2022-02-10 18:53:41 -0500

    giving up and switching to board game inventory

--- src/PokeList.js
+++ src/PokeList.js
@@ -1,4 +1,13 @@
 import Pokemon from './Pokemon.js';
+import React, { Component, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getPokemon } from './services/fetch-utils.js';
+
+// function component
+export function __PokeList({ pokemon }) {
+  // this.setState({ count: count + 1 })
+  const [count, setCounter] = useState(0);
+}
 
 export default function PokeList({ pokemon }) {
   return (
